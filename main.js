6db01dc7896a372f7e0eefa8d11f431c0b34351b
CodinGitHub <gms2programador@gmail.com>
2022-05-27 05:18:59 -0500

    script for video

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-let email = 'example@gmail.com';
+let emailElement = document.getElementById('email');
+let emailError = document.getElementById('email-error');
+let passElement = document.getElementById('pass');
+let showCheck = document.getElementById('show');
+let button = document.getElementById('submit');
+let passError = document.getElementById('pass-error');
 
-// Tiene el signo @ ?
-// Tiene espacios?
-// Tiene \
+button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    validateEmail(emailElement.value, emailError);
+    validatePass(passElement.value, passError); 
+});
 
-let regExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+function validateEmail(email, divError){
+    let regExpEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
-let emailError = regExp.test(email);
+    if(regExpEmail.test(email) == true){
+        divError.style.visibility = 'hidden';
+    }else{
+        divError.style.visibility = 'visible';
+    }
+}
 
-console.log(emailError)
+function validatePass(password, divError){
+    let regExpPass = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/g;
+
+    if(regExpPass.test(password) == true){
+        divError.style.visibility = 'hidden';
+    }else{
+        divError.style.visibility = 'visible';
+    }
+}
+
+showCheck.addEventListener('click', (event)=>{
+    if (event.target.checked == true){
+        passElement.type = 'text';
+    }else{
+        passElement.type = 'password';
+    }
+});
